0259389ca21f5c329e614286361c33161667a8ac
Peter Bray <petertheyeti@gmail.com>
2019-02-11 15:20:58 -0800

    Redirect if 404.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,6 @@
 import React from "react";
-import Page from "../components/Page";
+import { Redirect } from "@reach/router";
 
-class NotFoundPage extends React.Component {
-  render() {
-    return (
-      <Page title="Not Found">
-        <p>Hey! You just hit a page that doesn't exist...</p>
-      </Page>
-    );
-  }
-}
+const NotFoundPage = () => <Redirect to="/" />;
 
 export default NotFoundPage;
