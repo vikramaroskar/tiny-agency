4e5ba0057c36d4b1f89f63a761d5b4ca593bc0d3
Peter Bray <petertheyeti@gmail.com>
2019-02-11 16:31:58 -0800

    Try to fix FOUC.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import Typography from "@material-ui/core/Typography";
 import { Alien } from "mdi-material-ui";
 import { Robot } from "mdi-material-ui";
 import logo from "../../images/logo.png";
+import Hidden from "@material-ui/core/Hidden";
 
 const styles = theme => ({
     angles: {
@@ -48,56 +49,58 @@ const styles = theme => ({
       },
     } = props;
     return (
-      <Page>
-        <SEO title={defaultTitle}>
-          <meta name="description" content={defaultDescription} />
-          <link rel="canonical" href={domain} />
-        </SEO>
-        <div className={classes.text}>
-          <a href={domain}>
-            <img src={logo} alt={company} className={classes.logo} />
-          </a>
-          <Typography
-            paragraph
-            color="primary"
-            gutterBottom
-            variant="h3"
-            component="span"
-          >
-            <span className={classes.angles}>&lt;</span> hi{" "}
-            <span className={classes.angles}>&gt;</span>
-          </Typography>
-          <Typography paragraph gutterBottom variant="body1" component="span">
-            {preamble}
-          </Typography>
-          <Typography paragraph gutterBottom variant="body1" component="span">
-            {defaultDescription}
-          </Typography>
-        </div>
-        <div className={props.classes.tabs}>
-          <Tabs
-            items={[
-              ["Our Products", <Robot />, <List items={products} />],
-              ["Our Services", <Alien />, <List items={services} />],
-            ]}
-          />
-        </div>
-        <div className={classes.text}>
-          <Typography paragraph gutterBottom variant="body1" component="span">
-            {postamble}
-          </Typography>
-          <Typography
-            paragraph
-            color="primary"
-            gutterBottom
-            variant="h3"
-            component="span"
-          >
-            <span className={classes.angles}>&lt;</span> {email}{" "}
-            <span className={classes.angles}>&gt;</span>
-          </Typography>
-        </div>
-      </Page>
+      <Hidden implementation="css">
+        <Page>
+          <SEO title={defaultTitle}>
+            <meta name="description" content={defaultDescription} />
+            <link rel="canonical" href={domain} />
+          </SEO>
+          <div className={classes.text}>
+            <a href={domain}>
+              <img src={logo} alt={company} className={classes.logo} />
+            </a>
+            <Typography
+              paragraph
+              color="primary"
+              gutterBottom
+              variant="h3"
+              component="span"
+            >
+              <span className={classes.angles}>&lt;</span> hi{" "}
+              <span className={classes.angles}>&gt;</span>
+            </Typography>
+            <Typography paragraph gutterBottom variant="body1" component="span">
+              {preamble}
+            </Typography>
+            <Typography paragraph gutterBottom variant="body1" component="span">
+              {defaultDescription}
+            </Typography>
+          </div>
+          <div className={props.classes.tabs}>
+            <Tabs
+              items={[
+                ["Our Products", <Robot />, <List items={products} />],
+                ["Our Services", <Alien />, <List items={services} />],
+              ]}
+            />
+          </div>
+          <div className={classes.text}>
+            <Typography paragraph gutterBottom variant="body1" component="span">
+              {postamble}
+            </Typography>
+            <Typography
+              paragraph
+              color="primary"
+              gutterBottom
+              variant="h3"
+              component="span"
+            >
+              <span className={classes.angles}>&lt;</span> {email}{" "}
+              <span className={classes.angles}>&gt;</span>
+            </Typography>
+          </div>
+        </Page>
+      </Hidden>
     );
   };
 
